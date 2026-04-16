25a75222bee663f5c19cced946bee938993898a5
violetea00-dot <violetea00-dot@github.com>
2026-04-17 01:54:33 +0900

    fix: add trailingSlash, install remark-parse

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/my-new-blog",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
